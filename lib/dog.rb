840e86c0f4f81752596f38a205f2a3e88704c5fb
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-10 10:32:37 -0700

    done with the assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    #setter method
+    def name=(value)
+        @this_dogs_name = value
+    end
 
+    #getter method
+    def name
+        #access the data from the @name instance variable and return it
+        @this_dogs_name
+    end
 end
+
+# By using the @ in front of the variable name, the variable that's created inside the setter method is availbale inside the getter method (and inside any other methods in our class).
+# Whenever we use dot notation in Ruby, we're calling a method on an object!
+
+odie = Dog.new
+odie.name = "Odie"
+
+puts odie.name
